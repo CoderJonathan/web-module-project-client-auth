1a0ae291412d4894b07e72865c4e4204b1a4e494
Jonathan Garcia <714JonathanReneGarcia@gmail.com>
2021-05-18 19:44:57 -0700

    MVP

---
+++ friends/src/App.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Link, Switch } from 'react-router-dom';
+import Login from './components/login';
+import FriendsList from './components/friendsList';
+import PrivateRoute from './components/privateRoute';
+import './App.css';
+
+function App() {
+
+  const logout = () => {
+    localStorage.removeItem('token')
+  };
+
+  return (
+    <Router>
+      <div className='App'>
+        <h1>Friends</h1>
+        <div>
+        <Link to="/Login">Login</Link>
+        </div>
+        {localStorage.getItem('token') && <Link to="/protected">Friends List</Link>}
+        <Link onClick={logout}>Logout</Link>
+        <Switch>
+          <PrivateRoute exact path="/protected" component={FriendsList} />
+          <Route path="/login" component={Login} />
+          <Route component={Login} />
+        </Switch>
+
+      </div>
+    </Router>
+
+
+  );
+}
+
+export default App;
